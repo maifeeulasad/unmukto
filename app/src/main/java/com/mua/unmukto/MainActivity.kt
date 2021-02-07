346b5157dded13903b2e45da6717acff63a6fb6d
maifeeulasad <maifeeulasad@gmail.com>
2021-02-07 21:37:51 +0600

    added a custom keyboard//finally - only limated to application

--- app/src/main/java/com/mua/unmukto/MainActivity.kt
+++ app/src/main/java/com/mua/unmukto/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.mua.unmukto
 
-import androidx.appcompat.app.AppCompatActivity
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnKeyboardActionListener {
+
+    private lateinit var ukvTest: UnmuktoKeyboardView
+    private lateinit var etTest: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ukvTest = findViewById(R.id.ukv_test)
+        etTest = findViewById(R.id.et_test)
+
+        val mKeyboard = Keyboard(this, R.xml.kbd_bn)
+        ukvTest.setKeyboard(mKeyboard)
+        ukvTest.setOnKeyboardActionListener(this)
+    }
+
+    override fun onPress(primaryCode: Int) {
+        Log.d("d--mua", "press : $primaryCode")
+    }
+
+    override fun onRelease(primaryCode: Int) {
+        Log.d("d--mua", "release : $primaryCode")
+    }
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray) {
+        if (primaryCode == -120) {
+            ukvTest.keyboard = Keyboard(this, R.xml.kbd_bn_shifted)
+        } else if (primaryCode == -121) {
+            ukvTest.keyboard = Keyboard(this, R.xml.kbd_bn)
+        }
+    }
+
+    override fun onText(text: CharSequence) {
+        etTest.setText(etTest!!.text.toString() + text.toString())
+    }
+
+    override fun swipeLeft() {
+        Log.d("d--mua", "left")
+    }
+
+    override fun swipeRight() {
+        Log.d("d--mua", "right")
+    }
+
+    override fun swipeDown() {
+        Log.d("d--mua", "down")
+    }
+
+    override fun swipeUp() {
+        Log.d("d--mua", "up")
     }
 }
---
+++ app/src/main/java/com/mua/unmukto/UnmuktoKeyboardView.kt
@@ -0,0 +1,7 @@
+package com.mua.unmukto
+
+import android.content.Context
+import android.inputmethodservice.KeyboardView
+import android.util.AttributeSet
+
+class UnmuktoKeyboardView(context: Context?, attrs: AttributeSet?) : KeyboardView(context, attrs) 
