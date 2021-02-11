69462972cbd320fa97a317ca5ca7e212e8c9e2b2
maifeeulasad <maifeeulasad@gmail.com>
2021-02-11 16:59:35 +0600

    clear gh-pages//wip

--- app/src/main/java/com/mua/unmukto/UnmuktoKeyboardView.kt
+++
@@ -1,7 +0,0 @@
-package com.mua.unmukto
-
-import android.content.Context
-import android.inputmethodservice.KeyboardView
-import android.util.AttributeSet
-
-class UnmuktoKeyboardView(context: Context?, attrs: AttributeSet?) : KeyboardView(context, attrs) 
